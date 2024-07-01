1010764a9364bf8773d077462f014d5276d1578c
Sudhamoy Dey <1928325@kiit.ac.in>
2024-07-02 00:01:33 +0530

    Checkpoint 7

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+console.log("NWXNWXNJNKLNM")
+
+
+const cityCard1=document.querySelector(".image-section");
+
+cityCard1.addEventListener("click",function(){
+  window.location.href="list.html";
+})
--- scripts/list.js
+++ scripts/list.js
@@ -1,5 +1,5 @@
-function toggleSection(sectionId) {
-  const section = document.getElementById(sectionId);
+/* function toggleSection(sectionId) {
+  const section = document.getElementById("content-1");
 
   if (section.style.display === "none") {
       section.style.display = "block";
@@ -7,4 +7,17 @@ function toggleSection(sectionId) {
       section.style.display = "none";
       section.scrollIntoView({ behavior: "smooth", block: "center"}); // Scroll to the section
   }
-}
+} */
+
+const section=document.getElementById("map");
+//adding event:
+section.addEventListener("click", (e) => {
+  console.log(e);
+  const s1=document.querySelector("#content-3");
+  if(s1.style.display==="block"){
+    s1.style.display="none";
+  }
+  else{
+    s1.style.display="block";
+  }
+});
